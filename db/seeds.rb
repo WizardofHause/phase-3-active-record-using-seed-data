a9e39dd695e6a6342c3895d47de746e5192a7622
WizardofHause <jonrhause@gmail.com>
2022-11-14 18:19:42 -0700

    commit tree d48d2295e0c56bfc05110cc3898e423ae7695046

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,19 @@
-# Add a console message so we can see output when the seed file runs
+# db/seeds.rb
+puts "Planting seeds..."
+
+# Game.create(title: "Breath of the Wild", platform: "Switch", genre: "Action-adventure", price: 60)
+# Game.create(title: "Final Fantasy VII", platform: "Playstation", genre: "RPG", price: 60)
+# Game.create(title: "Mario Kart", platform: "Switch", genre: "Racing", price: 60)
+# Game.create(title: "Candy Crush Saga", platform: "Mobile", genre: "Puzzle", price: 0)
+
+50.times do
+    Game.create(
+        title: Faker::Cannabis.strain,
+        genre: Faker::Cannabis.type,
+        platform: Faker::Cannabis.buzzword,
+        price: rand(0..1000)
+    )
+end
+
+
+puts "Grow baby grow! 🌱"
